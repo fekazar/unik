146766e1d8bd61656b52eb52fb08c6576d40e3ed
Fyodor <fekazar@gmail.com>
2024-05-25 00:31:46 +0300

    Better consumers

--- project/email-send-consumer.py
+++ project/email-send-consumer.py
@@ -1,17 +1,18 @@
-import datetime
 import pika, sys, os
 
+EMAIL_QUEUE = 'email'
 ERROR_QUEUE = 'errorsqueue'
 ERROR_EXCHANGE = 'errorexchange'
-LOGS_QUEUE = 'logs'
-LOG_FILE = "messages.log"
 
-log_file = open(LOG_FILE, 'a')
+clients = []
+with open('clients.txt') as clients_file:
+    for line in clients_file:
+        clients.append(line.strip())
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-
 channel = connection.channel()
-channel.queue_declare(queue=LOGS_QUEUE)
+
+channel.queue_declare(queue=EMAIL_QUEUE)
 channel.queue_declare(queue=ERROR_QUEUE)
 
 channel.exchange_declare(exchange=ERROR_EXCHANGE,
@@ -20,17 +21,19 @@ channel.exchange_declare(exchange=ERROR_EXCHANGE,
 channel.queue_bind(exchange=ERROR_EXCHANGE,
                    queue=ERROR_QUEUE)
 
+print("Will send emails to these clients:", clients)
+
 def main():
     def callback(ch, method, properties, body):
         try:
-            log_file.write('Received: ' + body.decode() + ' at: ' + str(datetime.datetime.now()) + '\n')
-            log_file.flush()
+            # todo: send email to clients from client list
+            raise RuntimeError("Test error")
         except Exception as e:
             channel.basic_publish(ERROR_EXCHANGE,
                                   routing_key=ERROR_QUEUE,
                                   body=str(e))
 
-    channel.basic_consume(queue=LOGS_QUEUE, on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue=EMAIL_QUEUE, on_message_callback=callback, auto_ack=True)
     channel.start_consuming()
 
 if __name__ == '__main__':
